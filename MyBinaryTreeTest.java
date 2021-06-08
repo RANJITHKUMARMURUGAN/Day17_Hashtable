5bf3bea5cd0aae43950fa1cf100d1e8588ec2c12
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-06-08 13:29:35 +0530

    [Ranjith] added MyBinaryTreeTest.java

---
+++ MyBinaryTreeTest.java
@@ -0,0 +1,22 @@
+package binary_tree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+public class MyBinaryTreeTest {
+
+	@Test
+	void given_3Numbers_When_Added_ToBinaryTree_ShoudReturnSizeThree() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(2, size);
+	}
+}
